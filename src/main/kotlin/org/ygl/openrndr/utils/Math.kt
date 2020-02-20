0b47552cef6e7ee046ce9e5982868b3b1f5a8010
Yann Le Gall <ygall@indeed.com>
2020-02-19 18:36:35 -0800

    updates the interpolation utility

--- src/main/kotlin/org/ygl/openrndr/utils/Math.kt
+++ src/main/kotlin/org/ygl/openrndr/utils/Math.kt
@@ -1,11 +1,10 @@
 package org.ygl.openrndr.utils
 
 
-
-inline fun <reified T: Number> T.linearInterpolate(start: T, end: T, newStart: T, newEnd: T): Double {
+inline fun <reified T: Number> T.rangeMap(start: T, end: T, newStart: T, newEnd: T): Double {
     val startDouble = start.toDouble()
     val endDouble = end.toDouble()
-    val position = this.toDouble().coerceIn(startDouble, endDouble)
-    val progress = position / (endDouble - startDouble)
-    return newStart.toDouble() + progress * (newEnd.toDouble() - newStart.toDouble())
+    val newStartDouble = newStart.toDouble()
+    val newEndDouble = newEnd.toDouble()
+    return newStartDouble + (newEndDouble - newStartDouble) * ((this.toDouble() - startDouble) / (endDouble - startDouble))
 }
