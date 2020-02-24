dad8ab452f693d38e1cc558b4632e88ba662e6d6
Yann Le Gall <ygall@indeed.com>
2020-02-23 16:31:42 -0800

    fixes bug in conversion to radians

--- src/main/kotlin/org/ygl/openrndr/utils/Degrees.kt
+++ src/main/kotlin/org/ygl/openrndr/utils/Degrees.kt
@@ -3,7 +3,7 @@ package org.ygl.openrndr.utils
 inline class Degrees(
         val value: Double
 ) {
-    fun toRadians() = Radians(value * 180.0 / Math.PI)
+    fun toRadians() = Radians(value * Math.PI / 180.0)
 }
 
-fun <T: Number> T.degrees(value: T) = Degrees(value.toDouble())
+val <T: Number> T.degrees: Degrees; get() = Degrees(this.toDouble())
