4d164ae624636c219f39f12843bd673b58d7183d
Yann Le Gall <ygall@indeed.com>
2020-02-28 18:08:07 -0800

    adds distance to Vector utility

--- src/main/kotlin/org/ygl/openrndr/utils/Vector.kt
+++ src/main/kotlin/org/ygl/openrndr/utils/Vector.kt
@@ -2,7 +2,9 @@ package org.ygl.openrndr.utils
 
 import org.openrndr.math.Vector2
 import kotlin.math.cos
+import kotlin.math.pow
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 fun <T: Number> vector2(x: T, y: T) = Vector2(x.toDouble(), y.toDouble())
 
@@ -14,4 +16,8 @@ fun Vector2.rotate(amount: Radians): Vector2 {
             x * cos(rads) - y * sin(rads),
             x * sin(rads) + y * cos(rads)
     )
+}
+
+fun <T: Number> Vector2.distanceFrom(x: T, y: T): Double {
+    return sqrt((this.x - x.toDouble()).pow(2) + (this.y - y.toDouble()).pow(2))
 }
