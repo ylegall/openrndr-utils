7282caa4773d101ef6bc520c55ee1b5de2a090d8
Yann Le Gall <ygall@indeed.com>
2020-04-16 10:21:42 -0700

    adds random utilities

--- src/main/kotlin/org/ygl/openrndr/utils/Color.kt
+++ src/main/kotlin/org/ygl/openrndr/utils/Color.kt
@@ -2,21 +2,6 @@ package org.ygl.openrndr.utils
 
 import org.openrndr.color.ColorRGBa
 
-fun randomColorRGBa(
-        r: Number? = null,
-        g: Number? = null,
-        b: Number? = null,
-        a: Number? = null
-) = ColorRGBa(
-        r?.toDouble() ?: randomDoubleColor(),
-        g?.toDouble() ?: randomDoubleColor(),
-        b?.toDouble() ?: randomDoubleColor(),
-        a?.toDouble() ?: randomDoubleColor()
-)
-
-private fun randomIntChannel() = (0..255).random()
-private fun randomDoubleColor() = (0..255).random() / 255.0
-
 fun Int.argb2rgba() = ColorRGBa(
         r = ((this shr 16) and 0xFF) / 255.0,
         g = ((this shr 8) and 0xFF) / 255.0,
--- src/main/kotlin/org/ygl/openrndr/utils/Random.kt
+++ src/main/kotlin/org/ygl/openrndr/utils/Random.kt
@@ -1,5 +1,6 @@
 package org.ygl.openrndr.utils
 
+import org.openrndr.color.ColorRGBa
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Circle
 import org.openrndr.shape.Rectangle
@@ -16,6 +17,16 @@ fun randomDouble(low: Number = 0.0, high: Number = 1.0): Double {
     return Random.nextDouble(low.toDouble(), high.toDouble())
 }
 
+fun randomFloat(until: Number): Float {
+    return Random.nextFloat() * until.toFloat()
+}
+
+fun randomFloat(from: Number, until: Number): Float {
+    val start = from.toFloat()
+    val range = until.toFloat() - start
+    return start + Random.nextFloat() * range
+}
+
 fun Circle.randomPoint(random: Random = Random.Default): Vector2 {
     val randomAngle = random.nextDouble(2 * PI)
     val randomRadius = random.nextDouble(radius)
@@ -30,3 +41,15 @@ fun Rectangle.randomPoint() = Vector2(
         Random.nextDouble(y, y + height)
 )
 
+fun randomColor(r: Number? = null, g: Number? = null, b: Number? = null) = ColorRGBa(
+        r?.toDouble() ?: Random.nextDouble(),
+        g?.toDouble() ?: Random.nextDouble(),
+        b?.toDouble() ?: Random.nextDouble()
+)
+
+fun randomColor(r: Number? = null, g: Number? = null, b: Number? = null, a: Number? = null) = ColorRGBa(
+        r?.toDouble() ?: Random.nextDouble(),
+        g?.toDouble() ?: Random.nextDouble(),
+        b?.toDouble() ?: Random.nextDouble(),
+        a?.toDouble() ?: Random.nextDouble()
+)
